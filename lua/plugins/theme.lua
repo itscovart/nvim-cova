00384fe3e872523c13b8f59d1992895b3c6551a4
ItsCovart <covart06@gmail.com>
2026-05-06 14:17:45 -0600

    Agregar rotacion de temas, para mayor dinamica

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,43 +1,104 @@
 return {
-  "folke/tokyonight.nvim",
 
-  priority = 1000,
+  -------------------------------------------------
+  -- TOKYONIGHT
+  -------------------------------------------------
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+  },
 
-  config = function()
+  -------------------------------------------------
+  -- ONEDARK
+  -------------------------------------------------
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+  },
 
-    require("tokyonight").setup({
-      style = "night",
+  -------------------------------------------------
+  -- CATPPUCCIN
+  -------------------------------------------------
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
 
-      transparent = false,
+  -------------------------------------------------
+  -- GRUVBOX MATERIAL
+  -------------------------------------------------
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+  },
 
-      on_colors = function(colors)
-        colors.bg = "#000000" -- fondo negro puro
-      end,
+  -------------------------------------------------
+  -- CONFIG PRINCIPAL
+  -------------------------------------------------
+  {
+    "folke/tokyonight.nvim", -- usamos este para ejecutar config
+    config = function()
 
-      on_highlights = function(hl, colors)
+      -------------------------------------------------
+      -- TOKYONIGHT SETUP
+      -------------------------------------------------
+      require("tokyonight").setup({
+        style = "night",
+        transparent = false,
 
-        hl.Normal = { bg = "#000000" }
-        hl.NormalNC = { bg = "#000000" }
-        hl.EndOfBuffer = { bg = "#000000" }
-        hl.SignColumn = { bg = "#000000" }
-        hl.LineNr = { bg = "#000000" }
+        on_colors = function(colors)
+          colors.bg = "#000000"
+        end,
 
-        -- ⭐ Cursor line
-        hl.CursorLine = {
-          bg = "#111111" -- gris oscuro elegante
+        on_highlights = function(hl, colors)
 
+          hl.Normal = { bg = "#000000" }
+          hl.NormalNC = { bg = "#000000" }
+          hl.EndOfBuffer = { bg = "#000000" }
+          hl.SignColumn = { bg = "#000000" }
+          hl.LineNr = { bg = "#000000" }
+
+          hl.CursorLine = { bg = "#111111" }
+
+          hl.CursorLineNr = {
+            fg = "#7aa2f7",
+            bold = true
+          }
+
+        end,
+      })
+
+      -------------------------------------------------
+      -- DEFAULT THEME
+      -------------------------------------------------
+      vim.cmd.colorscheme("tokyonight")
+
+      -------------------------------------------------
+      -- THEME SWITCHER
+      -------------------------------------------------
+      vim.keymap.set("n", "<leader>tt", function()
+        local themes = {
+          "tokyonight",
+          "onedark",
+          "catppuccin-mocha",
+          "gruvbox-material",
         }
 
-        hl.CursorLineNr = {
-          fg = "#7aa2f7", -- azul bonito estilo Tokyonight
-          bold = true
+        -- guarda índice global
+        vim.g.theme_index = (vim.g.theme_index or 0) + 1
 
-        }
+        if vim.g.theme_index > #themes then
+          vim.g.theme_index = 1
+        end
 
-      end,
-    })
+        local theme = themes[vim.g.theme_index]
 
-    vim.cmd.colorscheme("tokyonight")
+        vim.cmd.colorscheme(theme)
+        print("Theme → " .. theme)
+      end)
 
-  end,
+    end,
+  },
+
 }
